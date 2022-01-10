6a19de0ff05b0ff9a5846aa65fd82a1a48564fa1
John Eric <azores.johneric28@gmail.com>
2022-01-10 21:54:36 +0800

    missing migration

---
+++ laravelbackend/database/migrations/2022_01_06_092143_event_info_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EventInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('event_info_table', function (Blueprint $table) {
+            // $table->id();
+            $table->unsignedBigInteger('id');
+            $table-> string("event_name");
+            $table-> date("event_date");
+            $table-> string("event_status");
+            $table-> string("event_address_line1");
+            $table-> string("event_address_line2");
+            $table-> string("event_barangay");
+            $table-> string("event_city");
+            $table-> string("event_province");
+            $table-> string("event_region");
+            $table->foreign('id')->references('id')->on('customer_info_table')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('event_info_table');
+    }
+}
